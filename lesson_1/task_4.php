51e0675f653e445ba3bab31ca41f7a38dc41d704
AlexLeo <masterlexx@mail.ru>
2021-10-12 01:06:48 +0300

    lesson_1_finished

---
+++ lesson_1/task_4.php
@@ -0,0 +1,12 @@
+<?php
+$title = 'Title';
+$date = date('Y-d-m');
+?>
+<html>
+<head>
+    <title><?php echo $title;?></title>
+</head>
+<footer>
+    <span><?php echo $date;?></span>
+</footer>
+</html>
