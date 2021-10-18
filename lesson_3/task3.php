2a6e2bf6d04d4e3d39c953d4b59caab5f59089f8
tax5773 <76212620+tax5773@users.noreply.github.com>
2021-10-18 17:08:42 +0300

    lesson_3_task_finished

--- lesson_3/task3.php
+++ lesson_3/task3.php
@@ -1,1 +1,17 @@
-3
+<?php
+$districts = array(
+	"Moscowskaya oblast" => array("Moscow", "Zelenograd", "Klin"),
+	"Leningradskaya oblast" => array("Sanct-Peterburg", "Vsevolzshk", "Paplovsk", "Kronshtad"),
+	"Ryazanskaya oblast" => array("Ryazan", "Kasimov", "Skopin")
+	);
+	
+	foreach ($districts as $key => $value) {
+    
+		echo "<br>{$key}:<br>";
+			foreach ( $value as $value2){
+				echo "{$value2}, ";
+			};		
+	};
+    //print_r($districts);
+
+	
