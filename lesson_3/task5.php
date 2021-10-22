ec7ab23dfdc3d99cce166b38419a1eb6e4273df2
tax5773 <76212620+tax5773@users.noreply.github.com>
2021-10-23 01:37:53 +0300

    lesson_3_task_finished

--- lesson_3/task5.php
+++ lesson_3/task5.php
@@ -1,1 +1,14 @@
-5
+<?php
+
+
+function replaceSpace($string){
+	
+    if (!is_string($string)) {
+        return "incorrect argument {$string}";
+    }else {
+		return preg_replace('/\s/', '_', $string);
+	};
+}
+
+echo replaceSpace('Проверка работы функции замены пробелов на нижнее подчеркивание');
+
